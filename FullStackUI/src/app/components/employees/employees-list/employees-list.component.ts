ec22eb16fdbdb595eae47cc2c9444f3e40452f23
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-06-07 01:06:12 +0500

    Employee component

--- FullStackUI/src/app/components/employees/employees-list/employees-list.component.ts
+++ FullStackUI/src/app/components/employees/employees-list/employees-list.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../../../models/employee.model';
 
 @Component({
   selector: 'app-employees-list',
   templateUrl: './employees-list.component.html',
-  styleUrls: ['./employees-list.component.css']
+  styleUrls: ['./employees-list.component.css'],
 })
-export class EmployeesListComponent {
+export class EmployeesListComponent implements OnInit {
+  employees: Employee[] = [
+    {
+      id: '1',
+      name: 'John',
+      email: 'john@gmail.com',
+      phone: 1234567890,
+      salary: 10000,
+      department: 'IT',
+    },
+    {
+      id: '2',
+      name: 'Jane',
+      email: 'jane@gmail.com',
+      phone: 9876543210,
+      salary: 12000,
+      department: 'HR',
+    },
+    {
+      id: '3',
+      name: 'Mike',
+      email: 'mike@gmail.com',
+      phone: 5555555555,
+      salary: 15000,
+      department: 'Finance',
+    },
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {
+    // Additional initialization logic can be added here
+  }
 }
