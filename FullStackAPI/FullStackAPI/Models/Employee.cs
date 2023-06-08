db33be25443e52d0fb1116fd4509b40fdc396dc7
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-06-08 22:10:22 +0500

    backend added

---
+++ FullStackAPI/FullStackAPI/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace FullStackAPI.Models
+{
+    public class Employee
+
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email{ get; set; }
+        
+        public long Phone { get; set; }
+
+        public long salary { get; set; }
+
+        public string Department { get; set; }
+    }
+}
