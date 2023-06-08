48eab702a4e312e7d0d45276daf73305323b5fcc
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-06-09 00:23:02 +0500

    migration added

---
+++ FullStackAPI/FullStackAPI/Data/FullStackDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FullStackAPI.Data
+{
+    public class FullStackDbContext : DbContext
+    {
+        public FullStackDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Models.Employee> Employees { get; set; }
+    }
+}
