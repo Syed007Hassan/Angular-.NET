64e2e7b967dbd7bc374ecee681e1a8262a2d8a71
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-06-10 13:19:13 +0500

    New project

--- FullStackAPI/FullStackAPI/Program.cs
+++ FullStackAPI/FullStackAPI/Program.cs
@@ -10,8 +10,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<FullStackDbContext>(options => 
-     options.UseSqlServer(builder.Configuration.GetConnectionString("FullstackConnectioString")));
+builder.Services.AddDbContext<FullStackDbContext>(options =>
+     options.UseSqlServer(builder.Configuration.GetConnectionString("FullstackConnectionString")));
 
 
 
